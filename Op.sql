fe37d728f06a12a525c08a0252879a6d1846b528
abijij <105397465+abijij@users.noreply.github.com>
2022-06-01 01:24:22 -0500

    Update Op.sql

--- Op.sql
+++ Op.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA `opdatabase` ;
-CREATE TABLE `opdatabase`.`clientes` (
+CREATE SCHEMA `Op` ;
+CREATE TABLE `Op`.`clientes` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `apellidoP` VARCHAR(45) NOT NULL,
@@ -8,5 +8,5 @@ CREATE TABLE `opdatabase`.`clientes` (
   `fechaRegistro` DATETIME NULL,
   `correo` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`));
-  INSERT INTO `opdatabase`.`clientes` (`id`, `nombre`, `apellidoP`, `apellidoM`, `rfc`, `fechaRegistro`, `correo`) VALUES ('1', 'Edgar', 'Gutierrez', 'Casañas', 'GUCE960715KA5', '2022-05-31 00:00:00', 'edgar.abijij24@gmail.com');
-INSERT INTO `opdatabase`.`clientes` (`id`, `nombre`, `apellidoP`, `apellidoM`, `rfc`, `fechaRegistro`, `correo`) VALUES ('2', 'Gabriela', 'Ponce', 'Hernadez', 'POHG99040929A', '2022-05-31 00:00:00', 'gavvpoce1824@gmail.com');
+  INSERT INTO `Op`.`clientes` (`id`, `nombre`, `apellidoP`, `apellidoM`, `rfc`, `fechaRegistro`, `correo`) VALUES ('1', 'Edgar', 'Gutierrez', 'Casañas', 'GUCE960715KA5', '2022-05-31 00:00:00', 'edgar.abijij24@gmail.com');
+INSERT INTO `Op`.`clientes` (`id`, `nombre`, `apellidoP`, `apellidoM`, `rfc`, `fechaRegistro`, `correo`) VALUES ('2', 'Gabriela', 'Ponce', 'Hernadez', 'POHG99040929A', '2022-05-31 00:00:00', 'gavvpoce1824@gmail.com');
